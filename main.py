a8f0d566de619b9f64c5e2471f58ba94d98a1765
Amendhahri53 <amendhahri53@gmail.com>
2022-12-01 21:03:11 +0100

    day-1

---
+++ main.py
@@ -0,0 +1,20 @@
+cals = []
+
+with open("input.txt", "r") as f:
+  contents = f.read().splitlines()
+  l = []
+  for line in contents:
+    if line == "":
+      cals.append(l)
+      l = []
+    else:
+      l.append(int(line))
+      if contents[-1] == line:
+        cals.append(l)
+
+sumd = [sum(x) for x in cals]
+print(max(sumd))
+
+# part 2
+sumd.sort(reverse=True)
+print(sumd[0] + sumd[1] + sumd[2])
